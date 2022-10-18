68ca7119fdcc29e87bead5bf024f43ec1a1389d8
Phil Metscher <philmetschermailt@gmx.de>
2022-10-18 12:38:11 +0200

    revert changes

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,22 @@
-import styled from "styled-components";
 import Head from "next/head";
+import styled from "styled-components";
 
-import Header from "../components/Header/Header";
-import CategoryHeadline from "../components/CategoryHeadline/CategoryHeadline";
-import CategoryListItem from "../components/CategoryListItem/CategoryListItem";
-
-import { exampleCategories, exampleListItems } from "../lib/db";
-
-export default function Home({ categories }) {
+export default function Home() {
   return (
-    <>
+    <div>
       <Head>
         <title>JustList</title>
         <meta name="description" content="JustList App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-      <main>
-        <section>
-          {exampleCategories.map((category) => (
-            <article key={category.id}>
-              <CategoryHeadline>{category.name}</CategoryHeadline>
-              <CategoryList>
-                {exampleListItems.map((listItem) =>
-                  listItem.categoryId === category.id ? (
-                    <CategoryListItem key={listItem.categoryId}>
-                      {listItem.name}
-                    </CategoryListItem>
-                  ) : (
-                    ""
-                  )
-                )}
-              </CategoryList>
-            </article>
-          ))}
-        </section>
-      </main>
-    </>
+      <Main>
+        <h1>My App</h1>
+      </Main>
+    </div>
   );
 }
 
-const CategoryList = styled.ul`
-  list-style-type: none;
+const Main = styled.main`
+  text-align: center;
 `;
