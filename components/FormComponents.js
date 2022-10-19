664a2d20d23a6d655ea037b27bdf73fa7f980335
Phil Metscher <philmetschermailt@gmx.de>
2022-10-19 09:58:28 +0200

    add some Form components

---
+++ components/FormComponents.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { IconList } from "./Icons";
+
+const Group = styled.div`
+  display: flex;
+  flex-flow: column;
+  gap: 8px;
+  position: relative;
+`;
+const Label = styled.label`
+  font-size: 1rem;
+  line-height: 1.375rem;
+  padding: 0 12px;
+`;
+const InputIcon = styled.span`
+  position: absolute;
+  left: ${({ iconBefore }) => (iconBefore ? "12px" : "unset")};
+  right: ${({ iconBefore }) => (iconBefore ? "unset" : "12px")};
+  bottom: 14px;
+  width: 24px;
+  height: 24px;
+  opacity: 0.5;
+`;
+
+function Input({ children, labelText, inputIcon, iconBefore = true }) {
+  return (
+    <Group>
+      <Label>{labelText}</Label>
+      <StyledInput iconBefore={iconBefore} placeholder={children} />
+      <InputIcon iconBefore={iconBefore}>
+        {inputIcon === "list" && <IconList />}
+      </InputIcon>
+    </Group>
+  );
+}
+
+const StyledInput = styled.input`
+  padding: 14px 12px;
+  background: var(--card-bg);
+  border: none;
+  border-radius: 13px;
+  padding: ${({ iconBefore }) =>
+    iconBefore ? "14px 12px 14px 46px" : "14px 46px 14px 12px"};
+`;
+
+function Select({
+  children,
+  labelText,
+  inputIcon,
+  iconBefore = true,
+  options,
+}) {
+  return (
+    <Group>
+      <Label>{labelText}</Label>
+      <select>
+        <option disabled selected>
+          {children}
+        </option>
+        {options.map((option) => (
+          <option>{option.name}</option>
+        ))}
+      </select>
+    </Group>
+  );
+}
+
+export { Input, Select };
