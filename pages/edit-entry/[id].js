8ee51152ebb527ec359fd5fefcc7b5d57a1aef4e
Phil Metscher <philmetschermailt@gmx.de>
2022-10-25 16:34:55 +0200

    changed the structure of the edit entry page a bit, removed the ability to add blank categories & list-items

--- pages/edit-entry/[id].js
+++ pages/edit-entry/[id].js
@@ -19,34 +19,91 @@ export default function EditEntry() {
   const router = useRouter();
   const { id } = router.query;
 
-  //get categories and events for categories & listItems
+  const startsWith = new RegExp("^[a-zA-Z]");
+
+  //######################
+  //GET THINGS FROM STORE
+  //######################
   const categories = useCategoriesStore((state) => state.categories);
+  const listItems = useListItemsStore((state) => state.listItems);
+
   const addCategory = useCategoriesStore((state) => state.addCategory);
   const editListItem = useListItemsStore((state) => state.editListItem);
-  //set error (category is in categories)
-  const [categoryExists, setCategoryExists] = useState(false);
-  //check if input-field with new category has value
-  const [categorySelectionAvailable, setCategorySelectionAvailable] =
+
+  //######################
+  //GET CURRENT LIST ITEM
+  //GET LIST ITEM CATEGORY
+  //######################
+  let listItem = {};
+  if (listItems) listItem = listItems.find((listItem) => listItem.id == id);
+
+  let listItemCategory = {};
+  if (listItem)
+    listItemCategory = categories.find(
+      (category) => category.id === listItem.categoryId
+    );
+
+  //######################
+  //SOME STATES
+  //######################
+  //variable to check if user has pressed enter on input field
+  const [pressedEnter, setPressedEnter] = useState(false);
+  //variable to check if input-field with new category has value
+  const [categoriesSelectionAvailable, setCategoriesSelectionAvailable] =
     useState(true);
-  //check if user has not pressed enter on input field
-  // (for mobile check purposes "Go" or "Enter")
-  const [enterInInput, setEnterInInput] = useState(false);
+  //variable to validate the "list item name" input field (not empty)
+  const [submitButtonReady, setSubmitButtonReady] = useState(true);
+  //variable to check if the new category exists in the already existing categories
+  const [categoryExistsInCategories, setCategoryExistsInCategories] =
+    useState(false);
 
-  const listItems = useListItemsStore((state) => state.listItems);
-  const listItem = listItems.find((listItem) => listItem.id == id) || {
-    name: "",
-  };
-  const listItemCategory = categories.find(
-    (category) => category.id === listItem.categoryId
-  );
-
+  //######################
+  //HANDLING FUNCTIONS
+  //######################
   function handleGoBack(event) {
     event.preventDefault();
-    if (!enterInInput) {
+    if (!pressedEnter) {
       router.push(`/`);
     } else {
-      setEnterInInput(false);
+      setPressedEnter(false);
     }
+  }
+
+  const handlePressEnter = (event) => {
+    //check if user has not pressed enter on input field (for mobile check purposes "Go" or "Enter")
+    event.keyCode == 13 ? setPressedEnter(true) : "";
+  };
+
+  function handleListItemInput(event) {
+    const value = event.target.value;
+
+    if (startsWith.test(value)) {
+      setSubmitButtonReady(true);
+    } else if ((value.length > 0) & value.startsWith(" ")) {
+      event.target.value = "";
+    } else {
+      setSubmitButtonReady(false);
+    }
+  }
+
+  function handleCategoryInput(event) {
+    const value = event.target.value;
+
+    if (startsWith.test(value)) {
+      setCategoriesSelectionAvailable(false);
+
+      if (categoryInCategories(event.target.value)) {
+        setCategoryExistsInCategories(true);
+      } else {
+        setCategoryExistsInCategories(false);
+      }
+    } else if ((value.length > 0) & value.startsWith(" ")) {
+      event.target.value = "";
+      setCategoriesSelectionAvailable(true);
+    } else {
+      setCategoriesSelectionAvailable(true);
+    }
+    setCategoryExistsInCategories(false);
   }
 
   function handleSubmit(event) {
@@ -55,61 +112,46 @@ export default function EditEntry() {
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
 
-    //check if user has not pressed enter on input field
-    // (for mobile check purposes "Go" or "Enter")
-    if (!enterInInput) {
-      let id = listItem.id,
-        name = listItem.name,
-        categoryId = listItem.categoryId,
-        somethingChanged = false;
+    let id = listItem.id,
+      name = listItem.name,
+      categoryId = listItem.categoryId,
+      somethingChanged = false;
 
+    if (!pressedEnter) {
       if (name !== data.itemName) {
         name = data.itemName;
         somethingChanged = true;
       }
-
       if (data.newCategory) {
-        // check if new category name exists in old categories names
-        if (
-          !categories.find((category) => category.name === data.newCategory)
-        ) {
-          const newCategoryId = nanoid();
+        const newCategoryId = nanoid();
 
-          addCategory(newCategoryId, data.newCategory);
-          categoryId = newCategoryId;
-          somethingChanged = true;
-        } else {
-          setCategoryExists(true);
-        }
-      } else {
-        if (categoryId !== data.itemCategory) {
-          categoryId = data.itemCategory;
-          somethingChanged = true;
-        }
+        addCategory(newCategoryId, data.newCategory);
+        categoryId = newCategoryId;
+
+        somethingChanged = true;
+      } else if (categoryId !== data.itemCategory) {
+        categoryId = data.itemCategory;
+
+        somethingChanged = true;
       }
 
+      //check if something has changed. if so redirect to list
       if (somethingChanged) {
         editListItem(id, name, categoryId);
         router.push(`/`);
       }
     } else {
-      setEnterInInput(false);
+      setPressedEnter(false);
     }
   }
 
-  function handleCategoryInput(event) {
-    const value = event.target.value;
-    value.length >= 1
-      ? setCategorySelectionAvailable(false)
-      : setCategorySelectionAvailable(true);
+  //######################
+  //HELPER FUNCTIONS
+  //######################
+  const categoryInCategories = (newCategoryName) =>
+    categories.find((category) => category.name === newCategoryName);
 
-    setCategoryExists(false);
-  }
-
-  const handlePressEnter = (event) =>
-    event.keyCode == 13 ? setEnterInInput(true) : "";
-
-  if (!categories || !listItems) {
+  if (!categories || !listItem) {
     return <p>Loading...</p>;
   }
 
@@ -128,7 +170,7 @@ export default function EditEntry() {
             name="itemName"
             labelText="Name des Eintrags"
             inputIcon="list"
-            handleChange={() => {}}
+            handleChange={(event) => handleListItemInput(event)}
             handleKeyPress={(event) => handlePressEnter(event)}
             value={listItem.name}
           >
@@ -145,7 +187,7 @@ export default function EditEntry() {
                   (category) => category.id !== listItem.categoryId
                 ),
               ]}
-              disabled={!categorySelectionAvailable}
+              disabled={!categoriesSelectionAvailable}
             />
           )}
           <Input
@@ -155,7 +197,7 @@ export default function EditEntry() {
             iconBefore={false}
             handleChange={(event) => handleCategoryInput(event)}
             handleKeyPress={(event) => handlePressEnter(event)}
-            error={categoryExists}
+            error={categoryExistsInCategories}
           >
             Kategorie-Name...
           </Input>
@@ -166,7 +208,13 @@ export default function EditEntry() {
             >
               <IconChevronLeft />
             </ButtonIcon>
-            <ButtonSmall isPrimary>
+            <ButtonSmall
+              isPrimary
+              disabled={!submitButtonReady}
+              onClick={() => {
+                return submitButtonReady;
+              }}
+            >
               speichern
               <IconChevronRight />
             </ButtonSmall>
