8896124096f98602fa191b8682aaf5281768cff0
Phil Metscher <philmetschermailt@gmx.de>
2022-11-08 09:29:40 +0100

    add edit-icon and link

--- components/OverviewListEntry.js
+++ components/OverviewListEntry.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 //Components
 import { PTag } from "./HtmlComponents";
+import { MdEdit } from "react-icons/md"; //Icons
 
 export default function OverviewListEntry({ id, children }) {
   return (
@@ -12,11 +13,18 @@ export default function OverviewListEntry({ id, children }) {
           <PTag>{children}</PTag>
         </ListEntryLink>
       </Link>
+      <Link href={"/edit-list/" + id} passHref>
+        <EditAnchor>
+          <MdEdit size="24px" />
+        </EditAnchor>
+      </Link>
     </ListEntry>
   );
 }
 
 const ListEntry = styled.li`
+  display: flex;
+  align-items: center;
   width: 100%;
   background: var(--card-bg);
   border-bottom: 1px solid var(--white);
@@ -26,7 +34,13 @@ const ListEntry = styled.li`
   }
 `;
 const ListEntryLink = styled.a`
-  display: block;
-  width: 100%;
+  display: inline-block;
+  width: calc(100% - 64px);
+  padding: 20px 0 19px 20px;
+`;
+const EditAnchor = styled.a`
+  display: inline-block;
+  width: 64px;
+  height: 64px;
   padding: 20px 20px 19px 20px;
 `;
