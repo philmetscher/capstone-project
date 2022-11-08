2c89e82d6226301a8cdf1e933613351e21a70907
Phil Metscher <philmetschermailt@gmx.de>
2022-11-08 11:40:07 +0100

    add drag icon

--- components/OverviewListEntry.js
+++ components/OverviewListEntry.js
@@ -3,11 +3,14 @@ import styled from "styled-components";
 
 //Components
 import { PTag } from "./HtmlComponents";
-import { MdEdit } from "react-icons/md"; //Icons
+import { MdOutlineDragHandle, MdEdit } from "react-icons/md"; //Icons
 
 export default function OverviewListEntry({ id, children }) {
   return (
     <ListEntry>
+      <DragWrapper>
+        <MdOutlineDragHandle size="24px" />
+      </DragWrapper>
       <Link href={"/list/" + id} passHref>
         <ListEntryLink>
           <PTag>{children}</PTag>
@@ -25,22 +28,30 @@ export default function OverviewListEntry({ id, children }) {
 const ListEntry = styled.li`
   display: flex;
   align-items: center;
+  gap: 10px;
   width: 100%;
   background: var(--card-bg);
   border-bottom: 1px solid var(--white);
+  padding: 0 20px;
 
   &:last-child {
     border-bottom-color: var(--card-bg);
   }
+
+  > * {
+    padding: 20px 0;
+  }
+`;
+const DragWrapper = styled.div`
+  width: 24px;
+  height: 64px;
 `;
 const ListEntryLink = styled.a`
   display: inline-block;
-  width: calc(100% - 64px);
-  padding: 20px 0 19px 20px;
+  width: calc(100% - 48px);
 `;
 const EditAnchor = styled.a`
   display: inline-block;
-  width: 64px;
+  width: 24px;
   height: 64px;
-  padding: 20px 20px 19px 20px;
 `;
